6f58e962dc4d215b96bd95ff1e319b5ee6314c63
root <root@debian-base.appalizer.net>
2014-01-14 23:16:28 +0100

    renamed files

---
+++ index.js
@@ -0,0 +1,8 @@
+//elasticsearch-plugin.js
+var EsPlugin = {
+        indexPost: function(postData) {
+            // do something with postData here
+            // so what do we get here ?
+            console.log (JSON.stringify(postData));
+        }
+    };
