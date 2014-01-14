b4527e28cf497592af39e90da2ed756dd1fad344
seti123 <stefan.thies@me.com>
2014-01-14 23:17:17 +0100

    Delete elasticsearch-plugin.js

--- elasticsearch-plugin.js
+++
@@ -1,8 +0,0 @@
-//elasticsearch-plugin.js
-var EsPlugin = {
-        indexPost: function(postData) {
-            // do something with postData here
-            // so what do we get here ?
-            console.log (JSON.stringify(postData));
-        }
-    };
