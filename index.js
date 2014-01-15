c04c196b37f06f721c69915792cd7fccae5addd9
root <root@debian-base.appalizer.net>
2014-01-15 20:00:01 +0100

    indexing on plugin activation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //elasticsearch-plugin.js
 var topics = require('../../src/topics');
+var posts = require('../../src/posts');
 var esHost = "localhost:9200"
 var postIndex = "nodebb_posts";
 var elasticsearch = require('elasticsearch');
@@ -7,9 +8,83 @@ var client = new elasticsearch.Client({
   host: esHost,
   log: 'trace'
 });
-
+var extend = require('util')._extend;
 var htmlToText = require('html-to-text');
 
+function createIndex (cb)
+{
+  client.indices.create ({
+        index : postIndex,
+        body: { mappings:
+                {
+                  nodebb_posts: {
+                    properties : {
+                     content : { type : "string" },
+                     title : { type : "string" },
+                     content_suggest : {
+                        type :     "completion"
+                      }
+                  }
+                 }
+              }
+          }
+    }, 
+    function (e, d) {    
+        if (! e) 
+            cb();
+        else
+          if (! /IndexAlreadyExistsException/im.test(e) ) // we can ignore if index is already there
+             console.log ("Error creating mapping " + e);
+          else
+              cb();
+    });
+  
+  
+}
+
+function indexAll ()
+{
+  console.log ('plugin activated, start indexing all posts ...')
+  topics.getAllTopics (-1, null, function (error, topics) 
+  {
+           //var bulkInstructions = [];
+           //var tpl  = { index:  { _index: postIndex, _type: postIndex, _id: 0 } };
+           for (var i=0; i<topics.length; i++)
+           {
+                //console.log (JSON.stringify (topics[i]));
+                console.log (topics[i].tid);
+                posts.getPostsByTid (topics[i].tid, new Date(0).getTime(), new Date().getTime(), function (err, posts) {
+                  console.log (JSON.stringify(posts));
+                  if (!err) 
+                  {
+                      
+                      posts.forEach ( function (i, e) {   insertToEs (i) } );
+                  } else console.log (err);
+
+                });
+           //     var cmd = extend ({}, tpl);
+                //console.log (JSON.stringyfy(tpoics));
+            //    cmd._id = topics.pid;
+            //    bulkInstructions.push (cmd);
+
+           }
+           //client.bulk ({
+           //   body: bulkInstructions
+           //}, function (e,d) {
+           //   console.log (e || d);
+           //
+           //});
+       });
+}
+
+var pluginActivated = function (pluginId) {
+    console.log ("ES activated" + pluginId);
+    if (pluginId == 'nodebb-plugin-elasticsearch') {
+        // we got something to do, its our plugin ...
+        // lets index all  topics just to be sure nobody is disappointed on search ...
+        createIndex ( indexAll );
+    } 
+}
 
 var EsPlugin = {
         indexPost: function(postData) {
@@ -17,10 +92,10 @@ var EsPlugin = {
             // so what do we get here ?
             console.log (JSON.stringify(postData));
             topics.getTopicField(postData.tid, 'title', function(err, title) {
-    				// use title
-    				postData.title = title;
-    				insertToEs (postData);
-			});
+            // use title
+            postData.title = title;
+            insertToEs (postData);
+      });
         },
         
         
@@ -32,6 +107,8 @@ var insertToEs = function (postData) {
             // collect complete post title, text
             // Upsert is used to either insert or update the document
             console.log ("insertToEs: " + JSON.stringify (postData));
+            if (! postData )
+              return;
             // Lets play around with autosuggestion
             // - if any word from post is typed, we want suggest post title
             // - if begin of Post tiel is typed as well ...
@@ -39,8 +116,9 @@ var insertToEs = function (postData) {
             client.index({
                   index: postIndex,
                   type: 'nodebb_posts',
-                  id: postData.pid,
-                  body: postData  
+                  id: postData.pid +'',
+                  body: { tid: postData.tid, title: postData.title, pid: postData.pid, content: postData.content, content_suggest: postData.content_suggest, timestamp: postData.timestamp }   
+                  //upsert: postDat
               }, 
               function (error, response) {
                         if (error) 
@@ -56,3 +134,4 @@ var insertToEs = function (postData) {
         }
 module.exports.indexPost = EsPlugin.indexPost
 module.exports.insertToEs = insertToEs
+module.exports.pluginActivated = pluginActivated
